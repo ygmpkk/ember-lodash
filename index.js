82356beec1ea41a5635761e023745c45180559ef
tchak <paul@chavard.net>
2016-03-09 22:28:56 +0100

    upgrade to use lodash 4.6.1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
   name: 'lodash',
 
   treeForAddon: function(tree) {
-    var lodashPath = path.dirname(require.resolve('lodash-es/lodash.js'));
+    var lodashPath = path.dirname(require.resolve('lodash-es'));
     var lodashTree = this.treeGenerator(lodashPath);
 
     var trees = mergeTrees([lodashTree, tree], {
